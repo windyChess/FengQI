704fcfec794b30e8248dbadee24502990c45778d
windyChess <huangfqao@163.com>
2019-11-26 17:48:50 +0800

    css 更改为 less

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+/*
+ * @Author: huangfengqi
+ * @Date: 2019-11-26 16:58:08
+ * @LastEditors: huangfengqi
+ * @LastEditTime: 2019-11-26 17:48:07
+ * @FilePath: \FengQI\src\app\app.component.ts
+ */
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'angular8-test';
+  
 }
