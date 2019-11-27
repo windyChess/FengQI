3e4627b168d05f0f063d72143f748a99acf80fb0
windyChess <huangfqao@163.com>
2019-11-27 17:54:44 +0800

    modify router

--- src/app/app-routinng.module.ts
+++ src/app/app-routinng.module.ts
@@ -2,15 +2,21 @@
  * @Author: huangfengqi
  * @Date: 2019-11-27 14:50:23
  * @LastEditors: huangfengqi
- * @LastEditTime: 2019-11-27 16:22:35
+ * @LastEditTime: 2019-11-27 17:51:51
  * @FilePath: \FengQI\src\app\app-routinng.module.ts
  */
 import {RouterModule,Routes} from '@angular/router';
 import { NgModule } from '@angular/core';
 import { MainComponent } from './main/main.component';
+import { ArticleComponent } from './article/article.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
 
 const routes:Routes = [
-    {path:'main',component:MainComponent,pathMatch:'full'}
+    {path:'main',component:MainComponent},
+    {path:'article',component:ArticleComponent},
+    {path:'projects',component:ProjectsComponent},
+    {path:'about',component:AboutComponent}
 ];
 
 @NgModule({
--- src/app/shared/selected.directive.ts
+++ src/app/shared/selected.directive.ts
@@ -2,7 +2,7 @@
  * @Author: huangfengqi
  * @Date: 2019-11-27 16:45:54
  * @LastEditors: huangfengqi
- * @LastEditTime: 2019-11-27 17:48:03
+ * @LastEditTime: 2019-11-27 17:50:23
  * @FilePath: \FengQI\src\app\shared\selected.directive.ts
  */
 import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
@@ -16,7 +16,6 @@ export class SelectedDirective {
               private rd:Renderer2) { }
 
   ngOnInit(){
-    console.log('--init--',this.el);
     const item = this.el.nativeElement.innerText;
     if(item === '首页'){
       this.rd.addClass(this.el.nativeElement,'a-selected');
@@ -34,10 +33,8 @@ export class SelectedDirective {
         this.rd.addClass(item,'a-unselected');
       }
     });
-
     this.rd.removeClass(this.el.nativeElement,'a-unselected');
     this.rd.addClass(this.el.nativeElement,'a-selected');
-    
   }
 
 }
